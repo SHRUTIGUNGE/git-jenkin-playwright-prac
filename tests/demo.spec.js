b71c037aa238112861526927fe1cfb1f1e55678a
SHRUTIGUNGE <shrutigunge5@gmail.com>
2025-10-07 11:37:02 +0530

    Update demo.spec.js
    i have added amezon

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -9,3 +9,7 @@ test("test 2", async ({ page }) => {
 test("test 3", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 });
+
+test("test 4", async({page})=>{
+  await page.goto("https://www.amazon.in/")
+})
